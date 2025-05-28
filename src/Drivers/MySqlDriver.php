b36c42dc0fe0b76cd0c8786e0c10a05fa6e0ee13
Gautam Singal <96416095+gautam-jbi@users.noreply.github.com>
2025-05-28 11:14:35 +0530

    Update MySqlDriver.php

--- src/Drivers/MySqlDriver.php
+++ src/Drivers/MySqlDriver.php
@@ -3,7 +3,7 @@
 namespace Larapack\DoctrineSupport\Drivers;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\PDOMySql\Driver;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver;
 use Larapack\DoctrineSupport\Managers\MySqlSchemaManager;
 
 class MySqlDriver extends Driver
